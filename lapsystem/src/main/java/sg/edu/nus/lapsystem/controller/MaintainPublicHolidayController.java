cc7c2f7fa45af5e6d4c081eb1e31714f4d0d1455
Team14SA48 <50819611+Team14SA48@users.noreply.github.com>
2019-05-22 15:20:16 +0800

    Add files via upload

---
+++ lapsystem/src/main/java/sg/edu/nus/lapsystem/controller/MaintainPublicHolidayController.java
@@ -0,0 +1,73 @@
+package sg.edu.nus.lapsystem.controller;
+
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import sg.edu.nus.lapsystem.model.PublicHoliday;
+import sg.edu.nus.lapsystem.repository.PublicHolidayRepository;
+
+@Controller
+public class MaintainPublicHolidayController {
+	private PublicHolidayRepository PBrepository;
+	private LocalDate LD;
+    @Autowired
+	public void setPBrepository(PublicHolidayRepository pBrepository) {
+		this.PBrepository = pBrepository;
+	}
+//    @RequestMapping(path="/")
+//	public String index() {
+//        return null;
+//    }
+    @RequestMapping(path = "/publicholidays/create", method = RequestMethod.GET)
+    public String createPublicHoliday(Model model) {
+        model.addAttribute("publicholiday", new PublicHoliday());
+        return "createpb";
+    }
+    @RequestMapping(path = "createpublicholidays", method = RequestMethod.POST)
+    public String savePulicHoliday(PublicHoliday publicholiday) {
+    	PBrepository.save(publicholiday);
+    	return "redirect:/publicholidays";
+    }
+    @RequestMapping(path = "publicholidays", method = RequestMethod.POST)
+    public String savePulicHolidayWhenEdit(PublicHoliday publicholiday) {
+		System.out.println(this.LD.toString());
+    	if(publicholiday.getDate()==this.LD) {
+
+    		PBrepository.save(publicholiday);
+        	return "redirect:/publicholidays";
+    	}else {
+            return "index";
+    	}
+    	
+    }
+    @RequestMapping(path = "/publicholidays", method = RequestMethod.GET)
+    public String getallPublicHoliday(Model model) {
+    	model.addAttribute("publicholidays", PBrepository.findAll());
+    	return "publicholidays";
+    }
+    
+    @RequestMapping(path = "/publicholidays/editpb/{date}", method = RequestMethod.GET)
+    public String editPublicHoliday(Model model,@PathVariable(value="date")LocalDate date) {
+    	PublicHoliday PB=PBrepository.findById(date).orElse(null);
+    	System.out.println(PB);
+    	this.LD=date;
+    	model.addAttribute("publicholiday", PB);
+    	model.addAttribute("publicholiday.date", PB.getDate());
+    	return "editpb";
+    }
+    @RequestMapping(path = "/publicholidays/delete/{date}", method = RequestMethod.GET)
+    public String deletePubliHoliday(@PathVariable(name="date")LocalDate date) {
+    	PublicHoliday pb=PBrepository.findById(date).orElse(null);
+    	PBrepository.delete(pb);
+    	return "redirect:/publicholidays";
+    }
+
+
+
+}
