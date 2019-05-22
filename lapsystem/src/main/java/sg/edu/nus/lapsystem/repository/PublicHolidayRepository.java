dff4f2c02fbc73caa9a2e4e3dfab51a3b908ab60
Team14SA48 <50819611+Team14SA48@users.noreply.github.com>
2019-05-22 15:24:21 +0800

    Add files via upload

--- lapsystem/src/main/java/sg/edu/nus/lapsystem/repository/PublicHolidayRepository.java
+++ lapsystem/src/main/java/sg/edu/nus/lapsystem/repository/PublicHolidayRepository.java
@@ -1,11 +1,11 @@
 package sg.edu.nus.lapsystem.repository;
 
-import java.util.Calendar;
+import java.time.LocalDate;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import sg.edu.nus.lapsystem.model.PublicHoliday;
 
-public interface PublicHolidayRepository extends JpaRepository<PublicHoliday, Calendar>{
+public interface PublicHolidayRepository extends JpaRepository<PublicHoliday,LocalDate>{
 
 }
